1ecb0faaaa14fc639985f7bc3614e649aa18a13b
ryusei <yamaryu1207@gmail.com>
2021-03-04 12:16:21 +0900

    テストページの追加

--- BlendBooth/limit.js
+++ BlendBooth/limit.js
@@ -1,13 +1,15 @@
-var limit = 1024 * 1024 * 1;
-var input = document.getElementById('audio');
+function init() {
+  var limit = 1024 * 1024 * 1;
+  var input = document.getElementById('audio');
 
-function sizeCheck() {
-  var file = input.files;
-  if (file.size > limit) {
-    alert('ファイルサイズは1MB以下にしてください');
-    input.value = '';
-    return ;
+  function sizeCheck() {
+    var file = input.files;
+    if (file.size > limit) {
+      alert('ファイルサイズは1MB以下にしてください');
+      input.value = '';
+      return;
+    }
   }
+  input.addEventListener('change', sizeCheck);
 }
-
-input.addEventListener('change',sizeCheck);
+window.onload = init;
