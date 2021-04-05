4d1803953682dd907eb2aa6f5953e49224879d3e
ryusei <yamaryu1207@gmail.com>
2021-04-05 16:52:48 +0900

    ページ遷移に関して修正

--- Silk-Reroad/map/panorama.js
+++ Silk-Reroad/map/panorama.js
@@ -1,5 +1,5 @@
 var element = document.getElementsByTagName("a-sphere");
 
 element[0].addEventListener("click", function() {
-  location.href = "https://ryusei-jp-y.github.io/ryusei/Silk-Reroad/quiz/quiz.html";
+  window.open("https://ryusei-jp-y.github.io/ryusei/Silk-Reroad/quiz/quiz.html");
 },false);
