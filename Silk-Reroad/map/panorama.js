28b944d24b3ff87034727ad03505a75036822b38
ryusei-jp-Y <79895620+ryusei-jp-Y@users.noreply.github.com>
2021-05-19 15:16:36 +0900

    Update panorama.js

--- Silk-Reroad/map/panorama.js
+++ Silk-Reroad/map/panorama.js
@@ -1,5 +1,5 @@
 var element = document.getElementsByTagName("a-sphere");
 
 element[0].addEventListener("click", function() {
-  window.open("https://ryusei-jp-y.github.io/ryusei/Silk-Reroad/quiz/quiz.html");
+  window.open("https://ryusei-jp-y.github.io/portfolio/Silk-Reroad/quiz/quiz.html");
 },false);
